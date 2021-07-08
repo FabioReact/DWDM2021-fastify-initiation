9b212fb6a104b1fd9950314a4a5a303a10b5fac2
FabioReact <fabio.react.dev@gmail.com>
2021-07-08 15:20:26 +0200

    Ajout fastify-jwt, POST /login

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { ObjectId } = require('mongodb') // https://docs.mongodb.com/drivers/node
 const argon2 = require('argon2') // https://www.npmjs.com/package/argon2
 const createError = require('http-errors') // https://www.npmjs.com/package/http-errors
 
+fastify.register(require('fastify-jwt'), {
+  secret: 'monsupersecretamoiestungangster'
+})
+
+
 // Connexion à la BDD
 fastify.register(require('fastify-mongodb'), {
 	forceClose: true,
@@ -165,6 +170,38 @@ fastify.get('/users', async (request, reply) => {
 	const collection = fastify.mongo.db.collection('users')
 	const result = await collection.find().toArray()
 	return result
+})
+
+fastify.post('/login', async (request, reply) => {
+	// Je récupère l'email et le password dans request,
+	// Je cherche si un utiliseur possede cet email,
+	// S'il existe, on vérifie que les password correspondent
+	// Sinon, on génère une erreur
+
+	const { email, password } = request.body
+	const collection = fastify.mongo.db.collection('users')
+
+	const userExists = await collection.findOne({ email })
+
+	if (!userExists) {
+		return createError(400, "Email et/ou mot de passe incorrect")
+	}
+
+	const match = await argon2.verify(userExists.password, password)
+
+	if (!match) {
+		return createError(400, "Email et/ou mot de passe incorrect")
+	}
+
+	// Je sais que l'email et le mot de passe sont corrects, j'envoi un token au client (permettant d'ainsi l'authentifier)
+	const token = fastify.jwt.sign({ id: userExists._id, role: userExists.role })
+
+	return { token }
+})
+
+fastify.get('/protected', async (request, reply) => {
+	// Si l'utilisateur ne m'envoie pas de token, je dois lui retourner une erreur
+	// Sinon, je lui retourne un objet contenant la propriété message avec Bienvenue comme valeur
 })
 
 fastify.get('/users/:id', async (request, reply) => {
